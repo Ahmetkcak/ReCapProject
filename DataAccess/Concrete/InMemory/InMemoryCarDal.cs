bbe9f16e4d517a89cd6e6e62cc9786e77a615f0d
AHMET KOCAK <f201213038@ktun.edu.tr>
2022-07-01 11:50:56 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,74 @@
+﻿using DataAccess.Abstract;
+using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+
+
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+
+                 new Car
+                {
+                    Id = 1, BrandId = 1, ColorId = 1, DailyPrice = 10000, Description = "Bardak", ModelYear = 2022
+                },
+                new Car
+                {
+                    Id = 2, BrandId = 2, ColorId = 1, DailyPrice = 9999, Description = "Kulaklik", ModelYear = 2019
+                },
+                new Car
+                {
+                    Id = 3, BrandId = 1, ColorId = 4, DailyPrice = 5000, Description = "Klavye", ModelYear = 2021
+                },
+                new Car
+                {
+                    Id = 4, BrandId = 1, ColorId = 3, DailyPrice = 5000, Description = "Telefon", ModelYear = 2020
+                }
+
+            };
+        }
+
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c=> c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int Id)
+        {
+            return _cars.Where(c => c.Id == Id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.Id = car.Id;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.Description = car.Description;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+        }
+    }
+}
---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Abstract
+{
+    public class Car : IEntity
+    {
+        public int Id { get; set; }
+
+        public int BrandId { get; set; }
+
+        public int ColorId { get; set; }
+
+        public int ModelYear { get; set; }
+
+        public decimal DailyPrice { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
